e8096a3f0e8e24860e2b0bb6dc06fa725a884a44
Miguel Tablado <miguel.tablado.leon@ibm.com>
2018-08-24 20:18:02 +0200

    icon and splash

--- src/env/env.dev.ts
+++ src/env/env.dev.ts
@@ -2,6 +2,6 @@ import { Environment } from "./env.interface";
 
 export const ENV: Environment = {
   mode: "DEV",
-  server_url: "http://localhost:8080",
-  server_api: "http://localhost:8080/private/api/v1"
+  server_url: "http://192.168.1.45:8080",
+  server_api: "http://192.168.1.45:8080/private/api/v1"
 }
